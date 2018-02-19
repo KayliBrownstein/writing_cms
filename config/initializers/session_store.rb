38eab4f507e3951c9deb09c81b9a0b010745f0f4
KayliBrownstein <kaylivbrownstein@gmail.com>
2018-02-18 19:30:44 -0500

    Initial commit

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_skeleton-project-rails_session'
+Rails.application.config.session_store :cookie_store, key: '_writing_cms_session'
