e0d5267f8388c07f8c2b57aacb80c6cc2982834c
KayliBrownstein <kaylivbrownstein@gmail.com>
2018-02-18 23:55:03 -0500

    Set up Google OAuth

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,28 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable,:recoverable, :rememberable, :trackable,
-         :validatable
+         :validatable, :omniauthable
+
+  def self.find_for_google_oauth2(access_token, signed_in_resource=nil)
+    data = access_token.info
+    user = User.where(email: data['email']).first
+    if user
+      return user
+    else
+      registered_user = User.where(email: access_token.info.email).first
+      if registered_user
+        return registered_user
+      else
+        user = User.create(
+          first_name: data["first_name"],
+          last_name: data["last_name"],
+          provider: access_token.provider,
+          email: data["email"],
+          photo: data["image"],
+          uid: access_token.uid ,
+          password: Devise.friendly_token[0,20],
+        )
+      end
+    end
+  end
 end
---
+++ db/migrate/20180219025620_add_oauth_columns_to_user.rb
@@ -0,0 +1,9 @@
+class AddOauthColumnsToUser < ActiveRecord::Migration[5.0]
+  def change
+    add_column :users, :provider, :string
+    add_column :users, :uid, :string
+    add_column :users, :first_name, :string
+    add_column :users, :last_name, :string
+    add_column :users, :photo, :string
+  end
+end
